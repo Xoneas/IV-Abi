fbf361b1d74fe8ac367053f79c54fd12124b221c
Xoneas <81169720+Xoneas@users.noreply.github.com>
2022-02-03 16:20:21 +0100

    Update regestrierung.php

--- 00PHP/06Formulare/regestrierung.php
+++ 00PHP/06Formulare/regestrierung.php
@@ -1,1 +1,47 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+<meta charset="utf-8">
+<title>Regestrierung</title>
+</head>
+<body>
 
+<?php
+
+$vorname = $_REQUEST["vorname"];
+$nachname = $_REQUEST["nachname"];
+$email = $_REQUEST["email"];
+$passwort = $_REQUEST["passwort"];
+$passwort2 = $_REQUEST["passwort2"];
+$geburtsdatum = $_REQUEST["geburtsdatum"];
+
+echo "<h1> Hallo $vorname $nachname </h1> ";
+
+echo "<p> Hier sind Ihre angegebenen Daten: </p>";
+
+echo " <table>
+<tr>
+<td> Email :</td>
+<td> $email </td>
+</tr>
+
+<tr>
+<td> Passwort </td>
+<td> $passwort </td>
+</tr>
+
+<tr>
+<td> Passwort2 : </td>
+<td> $passwort2 </td>
+</tr>
+
+<tr>
+<td> Geburtsdatum: </td>
+<td> $geburtsdatum </td>
+</tr>
+</table>
+";
+?>
+
+</body>
+</html>
