9ea9b97632a739dabd9e59d46388d99e73b1f822
Xoneas <81169720+Xoneas@users.noreply.github.com>
2022-02-03 18:24:33 +0100

    Update While.php

--- 00PHP/02While Schleife/While.php
+++ 00PHP/02While Schleife/While.php
@@ -2,10 +2,16 @@
 <html lang="de">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Titel</title>
+    <title>while schleife</title>
   </head>
   <body>
-
+<?php
+    $i = 0;
+    
+    while ($i < 10) {
+      echo "$i<br>;
+      $i++;
+    }
+    ?>
   </body>
 </html>
