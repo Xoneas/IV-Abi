bb3b07faffbfdbc76a1e78454dd1642d878139e2
Xoneas <81169720+Xoneas@users.noreply.github.com>
2022-02-03 18:31:02 +0100

    Create Berechnung.php

---
+++ 00PHP/05Berechnung/Berechnung.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <title>Berechnung</title>
+  </head>
+  <body>
+<?php
+    
+    $preis = 100;
+    
+    $preis_ohne_mwst = $preis *(1 - 0.19);
+    
+      echo "Preis: $preis;
+      echo "<br>";
+      echo "Preis neu: $preis_ohne_mwst";
+      
+      
+    ?>
+    
+    
+  </body>
+</html>
